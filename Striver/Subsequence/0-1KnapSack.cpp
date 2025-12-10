0799fa3b775899f2b70badb0d6d02d065d72d6b5
Shobhit150 <vshobhit150@gmail.com>
2025-12-11 05:20:30 +0530

    knapsack

---
+++ Striver/Subsequence/0-1KnapSack.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    
+    int bruteForce(vector<int>& wt, vector<int> &val, int n, int i, int currLeft) {
+        if(currLeft < 0) {
+            return INT_MIN/2;
+        }
+        if(i==n) {
+            return 0;
+        }
+        
+
+        int skip = bruteForce(wt, val, n, i+1, currLeft);
+        int take = val[i] + bruteForce(wt, val, n, i+1, currLeft - wt[i]);
+
+        return max(skip, take);
+    }
+    int memo(vector<int>& wt, vector<int> &val, int n, int i, int currLeft, vector<vector<int>> &dp) {
+        if(currLeft < 0) {
+            return INT_MIN/2;
+        }
+        if(i==n) {
+            return 0;
+        }
+        if(dp[i][currLeft] != -1) return dp[i][currLeft];
+        
+
+        int skip = memo(wt, val, n, i+1, currLeft, dp);
+        int take = val[i] + memo(wt, val, n, i+1, currLeft - wt[i], dp);
+
+        return dp[i][currLeft] = max(skip, take);
+    }
+    int tab(vector<int>& wt, vector<int> &val, int n, int W)
+    int KnapSack(vector<int>& wt, vector<int> &val, int n, int W) {
+        vector<vector<int>> dp(n+1, vector<int>(W+1,-1));
+        return memo(wt, val, n, 0, W,dp);
+    }
+};
+
+
+int main() {
+    vector<int> wt = {1, 3, 4, 5};
+    vector<int> val = {1, 4, 5, 7};
+    int W = 7;
+    int n = wt.size();
+
+    Solution s;
+    cout << "Ans: " << s.KnapSack(wt, val, n, W);
+
+    return 0;
+}
--- Striver/Subsequence/CountPartitionsWithGivenDifference.cpp
+++ Striver/Subsequence/CountPartitionsWithGivenDifference.cpp
@@ -1,10 +1,58 @@
 #include <iostream>
 using namespace std;
 
+// restSum = total - currentSum
+// restSum - currentSum = total - 2*currentSum
+// diff = total - 2*currentSum
+// 
 class Solution {
     public:
-    int countPartitions(int n, int diff, vector<int>& arr) {
+    int bruteForce(vector<int>& arr, int i, int currentSum, int total, int diff) {
         
+        if(i==arr.size()) {
+            if(total - (2*currentSum) == diff) return 1;
+            return 0;
+        }
+
+        int take = bruteForce(arr, i+1, currentSum + arr[i], total, diff);
+        int skip = bruteForce(arr, i+1, currentSum, total, diff);
+        return take + skip;
+    }
+    int memo(vector<int>& arr, int i, int currentSum, int total, int diff, vector<vector<int>>& dp) {
+        
+        if(i==arr.size()) {
+            if(total - (2*currentSum) == diff) return 1;
+            return 0;
+        }
+        if(dp[i][currentSum] != -1) return dp[i][currentSum];
+
+        int take = memo(arr, i+1, currentSum + arr[i], total, diff, dp);
+        int skip = memo(arr, i+1, currentSum, total, diff, dp);
+        return dp[i][currentSum] = take + skip;
+    }
+    int tab(vector<int>& arr, int n, int diff, int total) {
+        int val = total - diff;
+        if (val < 0 || val % 2 != 0) return 0;
+        int target = val / 2;
+        vector<vector<int>> dp(n+1, vector<int>(target+1, 0));
+        dp[n][(total - diff)/2] = 1;
+        for(int i=n-1;i>=0;i--) {
+            for(int s=0;s<target;s++) {
+                int skip = dp[i+1][s];
+                int take = 0;
+                if(s+arr[i] < total) {
+                    take = dp[i+1][s+arr[i]];
+                }
+                dp[i][s] = skip + take;
+            }
+        }
+        return dp[0][0];
+    }
+    int countPartitions(int n, int diff, vector<int>& arr) {
+        int total = 0;
+        for(int& num: arr) total += num;
+        vector<vector<int>> dp(n+1, vector<int>(total+1, -1));
+        return memo(arr, 0, 0, total, diff, dp);
     }
 };
 
