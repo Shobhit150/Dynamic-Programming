110d279692eb328c32dfd35d04b3600006c5d6c9
Shobhit150 <vshobhit150@gmail.com>
2025-12-21 20:10:54 +0530

    restructuring

---
+++ dp/Striver/Strings/WildcardMatching.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+    bool bruteForce(string s, string p, int i, int j) {
+        
+    }
+    bool isMatch(string s, string p) {
+        
+    }
+};
+
+int main() {
+    Solution s1;
+    string s = "adceb";
+    string t = "*a*b";
+    cout << s1.isMatch(s,t);
+}
